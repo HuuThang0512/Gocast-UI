62f0ec1a10449e1d65c935541f2103d58c05d340
HuuThang <huuthangblue0512@gmail.com>
2023-08-24 13:14:06 +0700

    responsive

--- js/app.js
+++ js/app.js
@@ -2,9 +2,24 @@ const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
 const toggles = $$(".toggle")
+const followBtns = $$(".follow-action .button")
+
 toggles.forEach((item) => {
     item.addEventListener("click", (e) => {
         const toggleParent = e.target.closest(".toggle")
         toggleParent.classList.toggle("active")
+    })
+})
+
+followBtns.forEach((item) => {
+    item.addEventListener("click", (e) => {
+        const btn = e.target.closest(".button")
+        const btnSpan = btn.querySelector("span")
+        btn.classList.toggle("active")
+        if (btn.classList.contains("active")) {
+            btnSpan.innerHTML = "Followed"
+        } else {
+            btnSpan.innerHTML = "Follow"
+        }
     })
 })
