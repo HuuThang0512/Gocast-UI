ce35e1d5258db40b9c020dbd5c4e372c57275e6f
HuuThang <huuthangblue0512@gmail.com>
2023-08-25 17:14:43 +0700

    add dark mode btn

--- js/app.js
+++ js/app.js
@@ -6,7 +6,34 @@ const followBtns = $$(".follow-action .button")
 const toTopBtn = $(".button-toTop")
 const menuBtn = $(".nav-menu-icon")
 const sidebar = $(".sidebar")
+const feedTabs = $$(".feed-tab-item")
+let feedTabActive = $(".feed-tab-item.active")
+const feedTabLine = $(".feed-tab-line")
+const sidebarClose = $(".sidebar-close")
+const themeBtn = $(".button-dark-mode")
 
+function changeLineTab() {
+    let feedTabActive = $(".feed-tab-item.active")
+    const feedTabLine = $(".feed-tab-line")
+    if (feedTabActive && feedTabLine) {
+        feedTabLine.style.left = feedTabActive.offsetLeft + "px"
+        feedTabLine.style.width = feedTabActive.offsetWidth + "px"
+    }
+}
+
+
+function loadTheme() {
+    let mediaQueryObj = window.matchMedia('(prefers-color-scheme: dark)');
+    let isDarkMode = mediaQueryObj.matches;
+    if (isDarkMode) {
+        $("html").classList.add("dark")
+    }
+}
+
+changeLineTab()
+loadTheme()
+
+// Click toggles
 toggles.forEach((item) => {
     item.addEventListener("click", (e) => {
         const toggleParent = e.target.closest(".toggle")
@@ -14,6 +41,7 @@ toggles.forEach((item) => {
     })
 })
 
+// Click Follow Button
 followBtns.forEach((item) => {
     item.addEventListener("click", (e) => {
         const btn = e.target.closest(".button")
@@ -27,12 +55,39 @@ followBtns.forEach((item) => {
     })
 })
 
+// Click Top Top Button
 toTopBtn.addEventListener("click", () => {
     window.scrollTo(0, 0)
 })
 
+// Click show/hide sidebar
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("active")
-    console.log(123123);
 })
-console.log(menuBtn);
+sidebarClose.addEventListener("click", () => {
+    sidebar.classList.remove("active")
+})
+
+document.addEventListener("click", (e) => {
+    const sidebarClick = e.target.closest(".sidebar")
+    const menuClick = e.target.closest(".nav-menu-iconP")
+    if (!sidebarClick && !menuClick && sidebar.classList.contains("active")) {
+        sidebar.classList.remove("active")
+    }
+})
+
+// Click Tab Items
+feedTabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        feedTabActive.classList.remove("active")
+        e.target.closest(".feed-tab-item").classList.add("active")
+        feedTabActive = e.target.closest(".feed-tab-item")
+        changeLineTab()
+    })
+})
+
+// Click change theme
+themeBtn.addEventListener("click", (e) => {
+    $("html").classList.toggle("dark")
+    // console.log(isDarkMode);
+})
