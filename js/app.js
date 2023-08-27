f92c7c217c02dd79e2eb7315b37399502ebb5362
HuuThang <huuthangblue0512@gmail.com>
2023-08-27 13:22:58 +0700

    Fix sidebar block margin body

--- js/app.js
+++ js/app.js
@@ -64,20 +64,20 @@ toTopBtn.addEventListener("click", () => {
 
 // Click show/hide sidebar
 menuBtn.addEventListener("click", () => {
-    // sidebar.classList.toggle("active")
-    sidebarBlock.classList.toggle("active")
+    sidebar.classList.toggle("active")
+    // sidebarBlock.classList.toggle("active")
 })
 sidebarClose.addEventListener("click", () => {
-    // sidebar.classList.toggle("active")
-    sidebarBlock.classList.remove("active")
+    sidebar.classList.toggle("active")
+    // sidebarBlock.classList.remove("active")
 })
 
 document.addEventListener("click", (e) => {
     let sidebarClick = e.target.closest(".sidebar")
     let menuClick = e.target.closest(".nav-menu-icon")
-    if (!sidebarClick && !menuClick && sidebarBlock.classList.contains("active")) {
-        // sidebar.classList.remove("active")
-        sidebarBlock.classList.remove("active")
+    if (!sidebarClick && !menuClick && sidebar.classList.contains("active")) {
+        sidebar.classList.remove("active")
+        // sidebarBlock.classList.remove("active")
     }
 })
 
